decfbd33e7768628fa62d3d0b8fd2c89a9d4139a
22duddu <163729768+22duddu@users.noreply.github.com>
2026-08-04 21:04:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 #--------------------------------------------
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8709203843:AAHP3QPRMwxWAZWWr6rPKrTPTKqfmZTj4J8")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 APP_ID = int(os.environ.get("APP_ID", "33964882")) #Your API ID from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "2b618df1eb69c19f08052490e0f9ee68") #Your API Hash from my.telegram.org
 #--------------------------------------------
@@ -29,7 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6908351235")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://20duddududdu1:20duddududdu1@cluster0.tq6n3ix.mongodb.net/?appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "100"))  # 0 means no expiry
